36f297c7d47ee4de8a675e556f81ef6d5f3fb1a0
user <you@example.com>
2022-04-01 17:04:01 +0600

    new

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
-<!--  
-
--->
+<?php
 
 $name = "Nurul Islam"; // datatype :  string
 $age = 30; // datatype : integer 
-$skill_has = ture // data type booliean
-$found = 3.5 // datatype double or float
+$skill_has = true; // data type booliean
+$found = 3.5; // datatype double or float
+
+echo $name;
