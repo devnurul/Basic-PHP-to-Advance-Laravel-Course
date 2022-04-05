45f2947e7a4eb1f529b27ad89cc1cd1d83228d7b
user <you@example.com>
2022-04-05 16:20:06 +0600

    assignemnt for class 4

--- Class4/index.php
+++ Class4/index.php
@@ -1,11 +1,28 @@
-<?php
+ <?php
+        // $currrent_year = date("M");
+      
+        // $inicial_year = $currrent_year - 50;
+        // for ($inicial_year ; $inicial_year <= $currrent_year ; $inicial_year++) { 
+        //         echo $inicial_year;
+        //         echo"<br>";
+  
+        // }
 
-$amount = 10;
+                
+                $currentMonth= date ("M");
+                $currentMonth = (int)$currentMonth;
+
+                var_dump($currentMonth);
+
+                // $currentYear= date ("Y");
+                // $currentYear = (int)$currentYear;
 
 
-$amount++;
+                // $last_day_month = cal_days_in_month(CAL_GREGORIAN, $currentMonth, $currentYear); 
 
-echo $amount;
+                // for ($i=1; $i <=  $last_day_month ; $i++) { 
+                //         echo $i;
+                //         echo "<br>";
+                // }
 
 
-?>
