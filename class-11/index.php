43e3df14caa9ebfd6fed4a7ea75ac12f04a111bc
devnurul <info@devnurul.me>
2022-05-14 13:00:17 +0600

    update

--- class-11/index.php
+++ class-11/index.php
@@ -7,6 +7,20 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
+  <style>
+
+.form_wrapper input[type="date"] {
+      width: 100%;
+      padding: 8px 10px 9px 35px;
+      height: 35px;
+      border: 1px solid #cccccc;
+      -webkit-box-sizing: border-box;
+      box-sizing: border-box;
+      outline: none;
+      -webkit-transition: all 0.30s ease-in-out;
+      transition: all 0.30s ease-in-out;
+  }
+  </style>
   <link rel="stylesheet" href="style.css">
   <title>Registation form</title>
 </head>
@@ -57,22 +71,55 @@
           }
         };
 
+/**
+ * 
+ * user name checker
+ */  
+        function userNameChecker($username) {
+          $partern = '/^\w{5,20}$/';
+          if (preg_match($partern, $username)) {
+              return true;
+          } else {
+              return false;
+          }
+      }
+      
+      function olddata($fieldname){
+        if (isset($_POST[$fieldname])){
+            echo $_POST[$fieldname];
+        }
+      };
+
+
+
 
   if (isset($_POST['submited'])) {
-    $fname       = $_POST["fname"];
-    $lname       = $_POST["lname"];
-    $email       = $_POST["email"];
-    $password    = $_POST["password"];
-    $age         = $_POST["age"];
-    $gender      = $_POST["gender"];
+    $fname    = $_POST["fname"];
+    $lname    = $_POST["lname"];
+    $email    = $_POST["email"];
+    $password = $_POST["password"];
+    $username = $_POST["username"];
+    $birthdas = $_POST["birthdas"];
+    if(isset($_POST["gander"])){
+      $gander = $_POST["gander"];
+    }
+   
+    if(isset($_POST["agreeStatus"])){
+      $agreeStatus      = $_POST["agreeStatus"];
+    }
+   
     $Country     = $_POST["Country"];
-    $agreeStatus = $_POST["agreeStatus"];
+    
 
      
-    if (empty($fname) || empty($lname)|| empty($email) || empty($password) || empty($age) ) {
+    if (empty($fname) || empty($lname)|| empty($email) || empty($password) || empty($age) || empty($username)) {
       $validationMessage = valided("All Field Are Requierd.", "red");
 
-    } elseif(emailCheck($email)==false){
+    }elseif(userNameChecker($username) == false){
+      $validationMessage = valided("User Name is not valid.", "#f5a42b");
+    }
+    
+    elseif(emailCheck($email)==false){
       $validationMessage = valided("Your Email is not valid.", "#f5a42b");
       
     }elseif(eduMailCheck($email) == false){
@@ -84,6 +131,7 @@
         
     else {
       $validationMessage = valided("You are done it nicely.", "#14cb6582");
+      $_POST = "";
       
     }
   }
@@ -108,46 +156,46 @@
         <div class="">
 
         
-          <form action="" method="POST">
+          <form action="" method="POST" autocomplete="off">
 
             <div class="input_field"> <span><i aria-hidden="true" class="fa-solid fa-user-check"></i></span>
-              <input type="text" name="username" placeholder="Username" />
+              <input type="text" name="username" placeholder="Username"  value="<?php olddata('username'); ?>"/>
             </div>
 
             <div class="input_field"> <span><i aria-hidden="true" class="fa fa-envelope"></i></span>
-              <input type="text" name="email" placeholder="Email" />
+              <input type="text" name="email" placeholder="Email" value="<?php olddata('email'); ?>" />
             </div>
 
             <div class="row clearfix">
               <div class="col_half">
                 <div class="input_field"> <span><i aria-hidden="true" class="fa fa-user"></i></span>
-                  <input type="text" name="fname" placeholder="First Name" />
+                  <input type="text" name="fname" placeholder="First Name" value="<?php olddata('fname'); ?>" />
                 </div>
               </div>
               <div class="col_half">
                 <div class="input_field"> <span><i aria-hidden="true" class="fa fa-user"></i></span>
-                  <input type="text" name="lname" placeholder="Last Name" />
+                  <input type="text" name="lname" placeholder="Last Name" value="<?php olddata('lname'); ?>" />
                 </div>
               </div>
             </div>
 
+            <!-- <div class="input_field"> <span><i class="fa-solid fa-stopwatch-20"></i></span>
+              <input type="text" name="age" placeholder="Your age"value="<?php //olddata('age'); ?>" />
+            </div> -->
+
             <div class="input_field"> <span><i class="fa-solid fa-stopwatch-20"></i></span>
-              <input type="text" name="age" placeholder="Your age" />
+              <input type="date" name="date"  value="<?php olddata('date'); ?>" />
             </div>
            
             <div class="input_field"> <span><i aria-hidden="true" class="fa fa-lock"></i></span>
-              <input type="password" name="password" placeholder="Password" />
+              <input type="password" name="password" placeholder="Password" value="<?php olddata('password'); ?>" />
             </div>
 
             <div class="input_field radio_option">
-              <!-- <input type="radio" name="gender" id="rd1 ">
-              <label for="rd1">Male</label>
-              <input type="radio" name="gender" id="rd2">
-              <label for="rd2">Female</label>
-              <input type="radio" name="gender" id="rd2">
-              <label for="rd2">Others</label> -->
-              <input type="radio" name="gander" value="male" id="male">
+              <input type="radio" name="gander" value="male" id="male"  value="<?php olddata('male'); ?>">
               <label for="male">Male</label>
+              <input type="radio" name="gander" value="female" id="female" value="<?php olddata('female'); ?>">
+              <label for="female">Female</label>
             </div>
 
             <div class="input_field select_option">
