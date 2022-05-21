b015e42d13a4b1c1f7d4b608de25af64c159251c
devnurul <info@devnurul.me>
2022-05-21 10:48:24 +0600

    Assignment 12

--- class-11/practice.php
+++ class-11/practice.php
@@ -1,11 +1,16 @@
 <?php
 
-$age = new DateTime('20.04.1990');
+// $age = new DateTime('20.04.1990');
 
-$today = new DateTime(date('d.m.y')) ;
+// $today = new DateTime(date('d.m.y')) ;
 
-$ageDiff = $today->diff($age);
+// $ageDiff = $today->diff($age);
 
-echo($ageDiff->y );
+// echo($ageDiff->y );
 
 
+for ($i=4; $i < 601 ; $i++) { 
+  echo $i+2;
+ 
+   echo "<br>";
+}
