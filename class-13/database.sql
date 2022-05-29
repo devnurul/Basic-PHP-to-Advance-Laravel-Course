54f7255ca4c7855ea17cf4abfd619eedd3853c03
devnurul <info@devnurul.me>
2022-05-29 20:09:48 +0600

    assignment

--- class-13/database.sql
+++ class-13/database.sql
@@ -1,1 +1,3 @@
-INSERT INTO 
+INSERT INTO user (username, age, phone, address) values ('nurul', '30', 0124579610, 'Kushtia Bangladesh');
+UPDATE user SET username ='nurul123' WHERE id = 1;
+DELETE FROM user WHERE id = 1;
