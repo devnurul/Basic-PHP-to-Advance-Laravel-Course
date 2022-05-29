c0e594f8d15c56cb8626f44b552e505d38e5d83f
devnurul <info@devnurul.me>
2022-05-29 20:30:45 +0600

    assignment7

--- class-13/database.sql
+++ class-13/database.sql
@@ -1,3 +1,11 @@
+-- user query 
+
 INSERT INTO user (username, age, phone, address) values ('nurul', '30', 0124579610, 'Kushtia Bangladesh');
 UPDATE user SET username ='nurul123' WHERE id = 1;
 DELETE FROM user WHERE id = 1;
+
+-- commernts query 
+
+INSERT INTO `comments`( `comment`, `post_id`) VALUES ('nice', 1)
+UPDATE `comments` SET `comment`='[value-3]' WHERE id = 1;
+DELETE FROM `comments` WHERE id = 1;
